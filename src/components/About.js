5cdaa6b5ecf7fcb10cbdc42330f1859e3274ea42
Nizam6239 <nizammalik6239@gmail.com>
2024-01-30 23:55:38 +0530

    Creating the context folder in which notes folder have the file NoteContentand NoteState coded

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, {useContext,useEffect} from 'react'
+import noteContext from '../context/notes/NoteContext';
+
+export const About = () => {
+    const a = useContext(noteContext)
+    useEffect(() => {
+        a.update();
+    // eslint-disable-next-line
+    }, [])
+    
+  return (
+    <div>This is about {a.state.name} and he is class {a.state.class}</div>
+  )
+}
+
