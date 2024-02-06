fd97bc5410a684012695b8cc5056fd0f97841f2a
Nizam6239 <nizammalik6239@gmail.com>
2024-02-06 17:40:21 +0530

    create the login page for the inotebook app

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -62,6 +62,7 @@ router.post(
     body("password", "Password cannot be blank").exists(),
   ],
   async (req, res) => {
+    let success = false
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -76,9 +77,10 @@ router.post(
       }
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
+        success = false;
         return res
           .status(400)
-          .json({ error: "Please try to login with correct credentials" });
+          .json({ success, error: "Please try to login with correct credentials" });
       }
       const data = {
         user: {
@@ -86,7 +88,8 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({ authtoken });
+      success = true;
+      res.json({ success,authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
